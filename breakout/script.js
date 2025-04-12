48f8d580a58063d93fad7a6564858ae9e1ef3d45
Nathan Boquiren <nathanjboquiren@gmail.com>
2025-04-11 20:15:18 -0400

    fixed bugs and stuff

--- breakout/script.js
+++ breakout/script.js
@@ -2,7 +2,7 @@
 
 let cl = console.log;
 
-// ===== Global Variables
+// ===== Global Variables =====
 
 let ball;
 let paddle;
@@ -11,18 +11,28 @@ let gameStarted = false;
 let lives = 5;
 let score = 0;
 
+// bounce animation variables
+let halo;
+let particles = [];
+
+let sfx = {
+  paddle: new Audio("sfx/paddle.wav"),
+  block: new Audio("sfx/block.wav"),
+  life: new Audio("sfx/life.wav"),
+};
+
 // ===== block variables =====
 
 let blocks = [];
 
 const blockColors = [
-  "#FF3131", // Electric Red
-  "#FF6B00", // Tangerine Orange
-  "#FFD700", // Golden Yellow
-  "#32FF6A", // Lime Green
-  "#00F0FF", // Cyan Blue
-  "#7A5CFF", // Indigo Purple
-  "#FF5CA2", // Hot Pink
+  "#FF3131",
+  "#FF6B00",
+  "#FFD700",
+  "#32FF6A",
+  "#00F0FF",
+  "#7A5CFF",
+  "#FF5CA2",
 ];
 
 let blockCol = 15;
@@ -30,13 +40,13 @@ let blockCol = 15;
 // ===== Classes =====
 
 class Block {
-  constructor(x, y, clr, width, height) {
+  constructor(x, y, clr, width, height, row) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
     this.clr = clr;
-    this.hit = false;
+    this.row = row;
   }
   create() {
     fill(this.clr);
@@ -54,7 +64,7 @@ class Paddle {
   constructor() {
     this.w = 200;
     this.h = 30;
-    this.x = mouseX - this.width / 2;
+    this.x = mouseX - this.w / 2;
     this.y = height - 200;
     this.br = this.h / 2;
   }
@@ -92,6 +102,58 @@ class Ball {
     circle(this.x, this.y, this.d);
   }
 }
+
+class Particle {
+  constructor(x, y, clr) {
+    this.x = x;
+    this.y = y;
+    this.vx = random(-5, 5);
+    this.vy = random(-5, 5);
+    this.clr = clr;
+    this.alpha = 255;
+  }
+  animate() {
+    this.x += this.vx;
+    this.y += this.vy;
+    this.alpha -= 5;
+
+    if (this.alpha <= 0) {
+      let i = particles.indexOf(this);
+      particles.splice(i, 1);
+    }
+  }
+  show() {
+    let clr = color(this.clr);
+    clr.setAlpha(this.alpha);
+    fill(clr);
+    circle(this.x, this.y, 10);
+  }
+}
+
+class paddleHalo {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.w = 200;
+    this.h = 30;
+    this.r = this.h / 2;
+    this.alpha = 200;
+  }
+
+  animate() {
+    this.x = mouseX - this.w / 2;
+    this.y -= 1;
+    this.w += 2;
+    this.h += 2;
+    this.r = this.h / 2;
+    this.alpha -= 10;
+  }
+  show() {
+    fill(255, this.alpha);
+    rect(this.x, this.y, this.w, this.h, this.r);
+  }
+}
+
 // ===== setup =====
 
 function setup() {
@@ -107,7 +169,8 @@ function setup() {
           i * blockHeight,
           blockColors[i],
           blockWidth,
-          blockHeight
+          blockHeight,
+          i + 1
         )
       );
     }
@@ -125,9 +188,10 @@ function draw() {
   // Text
   textFont("Press Start 2P");
   let centerMsg = lives > 0 ? "Click Mouse to Start Game" : "GAME OVER";
-
+  let txtClr = color(255);
+  txtClr.setAlpha(255);
+  fill(txtClr);
   if (!gameStarted) {
-    fill(255);
     textAlign(CENTER);
     textSize(30);
     text(centerMsg, width / 2 - 250, height / 2 - 50, 500);
@@ -156,9 +220,25 @@ function draw() {
   checkPaddleBounce(ball, paddle);
   checkWallBounce(ball);
   checkBlockBounce(ball, blocks);
+
+  // bounce animations
+
+  particles.forEach((particle) => {
+    particle.animate();
+    particle.show();
+  });
+
+  if (halo) {
+    halo.animate();
+    halo.show();
+  }
+
+  if (blocks.length === 0) {
+    gameStarted = false;
+  }
 }
 
-// Make lives adn score strings
+// Make lives and score strings
 
 function heartStr() {
   let hearts = [];
@@ -169,24 +249,32 @@ function heartStr() {
 }
 
 function scoreStr() {
-  return score < 100 ? "00" + score : score;
+  return String(score).padStart(3, "0");
 }
 
 // bounce check for paddle, wall, and blocks
 
 function checkPaddleBounce(b, p) {
-  let pTopEdge = p.y + p.h / 2;
-  let pLeftEdge = p.x;
-  let pRightEdge = p.x + p.w;
+  let pTop = p.y;
+  let pBottom = p.y + p.h;
+  let pLeft = p.x;
+  let pRight = p.x + p.w;
 
-  let bBottomEdge = b.y + b.r;
-  let bTopEdge = b.y;
-  let bXCenter = b.x + b.r;
+  let bTop = b.y - b.r;
+  let bBottom = b.y + b.r;
+  let bLeft = b.x - b.r;
+  let bRight = b.x + b.r;
 
-  let xAligned = pLeftEdge <= bXCenter && pRightEdge >= bXCenter;
-  let yAligned = pTopEdge >= bBottomEdge && bTopEdge >= pTopEdge - b.d;
-  if (xAligned && yAligned) {
-    let section = bXCenter - pLeftEdge;
+  let isColliding =
+    bBottom >= pTop &&
+    bTop <= pBottom &&
+    bRight >= pLeft &&
+    bLeft <= pRight &&
+    b.vy > 0;
+
+  if (isColliding) {
+    let bXCenter = b.x;
+    let section = bXCenter - pLeft;
 
     let leftBounce = section < p.w / 3;
     let middleBounce = section >= p.w / 3 && section < (p.w / 3) * 2;
@@ -198,12 +286,18 @@ function checkPaddleBounce(b, p) {
       b.vx *= Math.abs(b.vx) === 15 ? 1 / 3 : 1;
     } else if (rightBounce) {
       b.vx = -15;
-    } else {
-      ("bounce");
     }
 
+    // let relativeIntersectX = b.x - (p.x + p.w / 2);
+    // let normalized = relativeIntersectX / (p.w / 2);
+    // let maxSpeed = 15;
+    // b.vx = normalized * maxSpeed;
+
     b.vy *= -1;
-    playPop(`paddle`);
+    b.y = p.y - b.r;
+    playSfx(`paddle`);
+
+    paddleAnimation(b.x, p.y);
   }
 }
 
@@ -254,33 +348,74 @@ function checkBlockBounce(b, blks) {
         b.y += distY > 0 ? overlapY : -overlapY;
       }
 
-      blks.splice(i, 1);
-      playPop(`block`);
+      let removedBlock = blks[i];
 
-      increaseScore();
+      blockAnimation(
+        removedBlock.x + removedBlock.width / 2,
+        removedBlock.y + removedBlock.height / 2,
+        removedBlock.clr
+      );
+
+      blks.splice(i, 1);
+
+      let rowComplete = checkRow(removedBlock, removedBlock.row);
+
+      playSfx(`block`);
+
+      if (rowComplete === true) {
+        increaseScore(50);
+        cl("row complete");
+      } else {
+        increaseScore(10);
+      }
     }
   }
 }
 
-function playPop(type) {
-  sfx = new Audio(`${type}.wav`);
-  sfx.play();
+// Check if row is complete
+function checkRow(currentBlk, rowNum) {
+  for (let i = 0; i < blocks.length; i++) {
+    if (blocks[i] !== currentBlk && blocks[i].row === rowNum) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// ===== Style =====
+
+// Play pop sfx
+function playSfx(type) {
+  if (sfx[type]) {
+    sfx[type].currentTime = 0;
+    sfx[type].play();
+  }
+}
+
+// Paddle and Block bounce animations
+function blockAnimation(x, y, clr) {
+  for (let i = 0; i < 100; i++) {
+    particles.push(new Particle(x, y, clr));
+  }
+}
+
+function paddleAnimation(x, y) {
+  halo = new paddleHalo(x, y);
 }
 
 // Score increase
-
-function increaseScore() {
-  score += 10;
+function increaseScore(amount) {
+  score += amount;
 }
 
 // Lives decrease
-
 function decreaseLives() {
   lives = constrain(lives - 1, 0, 5);
 
   gameStarted = false;
 
   if (lives !== 0) {
+    playSfx(`life`);
     ball = new Ball();
   }
 }

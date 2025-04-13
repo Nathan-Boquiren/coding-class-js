c5cd2aa9bffd02ccfce2f520c066d4d72db599fc
Nathan Boquiren <nathanjboquiren@gmail.com>
2025-04-13 15:38:30 -0400

    regular polygon

--- p5-projects/p5-11.js
+++ p5-projects/p5-11.js
@@ -2,91 +2,61 @@
 
 let cl = console.log;
 
-let blocks = [];
+function setup() {
+  createCanvas(windowWidth, windowHeight);
 
-class Block {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.direction;
-    this.v = 60;
-    this.s = 60;
-    this.clr = color(255);
-  }
-
-  move() {
-    if (this.direction === "UP") {
-      this.y -= this.v;
-    } else if (this.direction === "DOWN") {
-      this.y += this.v;
-    } else if (this.direction === "LEFT") {
-      this.x -= this.v;
-    } else if (this.direction === "RIGHT") {
-      this.x += this.v;
-    }
-    // this.x = constrain(this.x, 0, width);
-    // this.y = constrain(this.y, 0, height);
-  }
-
-  draw() {
-    stroke(20);
-    strokeWeight(2);
-    fill(this.clr);
-    square(this.x, this.y, this.s, 10);
+  for (let i = 28; i >= 3; i--) {
+    polygons.push(new Polygon(0, 0, i ** 2.1, i, i % 2 === 0));
   }
 }
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(20);
+let polygons = [];
 
-  for (let i = 0; i < 8; i++) {
-    blocks.push(new Block(60, i * 60 + 120));
+class Polygon {
+  constructor(x, y, s, p, fill) {
+    this.x = x;
+    this.y = y;
+    this.s = s;
+    this.p = p;
+    this.a = radians(360 / p);
+    this.r = 0;
+    this.rSpeed = 1 / pow(this.p, 2);
+    this.fill = fill;
+  }
+
+  create() {
+    if (this.fill) {
+      fill(255);
+    } else {
+      fill(20);
+    }
+    beginShape();
+    for (let i = 0; i < this.p; i++) {
+      let angle = i * this.a;
+      let xOff = sin(angle) * this.s;
+      let yOff = cos(angle) * this.s;
+      vertex(this.x + xOff, this.y + yOff);
+    }
+    endShape(CLOSE);
+  }
+
+  rotate() {
+    rotate((this.r += this.rSpeed));
   }
 }
 
 function draw() {
   background(20);
-  createGrid();
+  stroke(255);
+  strokeWeight(2);
+  noFill();
 
-  for (const b of blocks) {
-    if (frameCount % 30 === 0) {
-      b.move();
-    }
+  translate(width / 2, height / 2);
 
-    b.draw();
-  }
-
-  for (let i = blocks.length - 1; i < blocks.length; i--) {
-    const b = blocks[i];
-  }
-}
-
-function keyPressed() {
-  let d;
-  if (keyCode === UP_ARROW || key === "w") {
-    d = "UP";
-  } else if (keyCode === DOWN_ARROW || key === "s") {
-    d = "DOWN";
-  } else if (keyCode === LEFT_ARROW || key === "a") {
-    d = "LEFT";
-  } else if (keyCode === RIGHT_ARROW || key === "d") {
-    d = "RIGHT";
-  }
-
-  for (const b of blocks) {
-    b.direction = d;
-  }
-}
-
-function createGrid() {
-  stroke(255, 100);
-
-  for (let i = 0; i <= width; i += 60) {
-    line(i, 0, i, height);
-  }
-
-  for (let j = 0; j <= height; j += 60) {
-    line(0, j, width, j);
-  }
+  polygons.forEach((p) => {
+    push();
+    p.rotate();
+    p.create();
+    pop();
+  });
 }
